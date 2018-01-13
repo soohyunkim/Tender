0077a0060789dca6fbd8b45c7c3019873309d08a
rwblickhan <rwblickhan@gmail.com>
2018-01-13 15:31:37 -0800

    Add stub for server

---
+++ backEnd/server.py
@@ -0,0 +1,19 @@
+from flask import Flask, abort
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    abort(404)
+
+#GET here to retrieve an event,
+#POST here to create a new event
+@app.route('/event')
+def event():
+    abort(404)
+
+#GET here to retrieve voting options,
+#POST here to submit votes
+@app.route('/vote')
+def vote():
+    abort(404)
