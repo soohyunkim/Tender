659813f49615f7ff547cdfb0d8f5a9a8c4826033
rwblickhan <rwblickhan@gmail.com>
2018-01-13 15:38:13 -0800

    Add methods to route decorators and conform to PEP-8

--- backEnd/server.py
+++ backEnd/server.py
@@ -2,18 +2,21 @@ from flask import Flask, abort
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     abort(404)
 
-#GET here to retrieve an event,
-#POST here to create a new event
-@app.route('/event')
+
+# GET here to retrieve an event,
+# POST here to create a new event
+@app.route('/event', methods=['GET', 'POST'])
 def event():
     abort(404)
 
-#GET here to retrieve voting options,
-#POST here to submit votes
-@app.route('/vote')
+
+# GET here to retrieve voting options,
+# POST here to submit votes
+@app.route('/vote', methods=['GET', 'POST'])
 def vote():
     abort(404)
