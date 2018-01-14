9a63e78696a9b351a8170ae8299651382e6a16d6
rwblickhan <rwblickhan@gmail.com>
2018-01-14 09:34:05 -0800

    Change incorrect mimetype in POST response

--- server.py
+++ server.py
@@ -78,7 +78,7 @@ def options():
 
         emails= []
         send_event_id_email(event_id, emails)
-        return Response(status=200, mimetype='application/json')
+        return Response(status=200)
     abort(405)
 
 
