c32e313ca60b735c2cca7f7b89cdfed09bd33214
Sherry Yuan <frostyyshadows@gmail.com>
2018-01-13 16:59:47 -0800

    making yelp api request

--- backEnd/server.py
+++ backEnd/server.py
@@ -2,6 +2,7 @@ from flask import Flask, abort, request, render_template, redirect, url_for
 from firebase import firebase
 import urllib
 import urllib2
+import json
 
 app = Flask(__name__)
 
@@ -18,6 +19,7 @@ def index():
 def event():
     if request.method == 'POST':
         # parse out parameters from POST request
+
         location = request.args.get("location")
         radius = request.args.get("radius")
         categories = request.args.get("categories")
@@ -28,9 +30,16 @@ def event():
         params = urllib.urlencode({'location': location, 'radius': radius, 'categories': categories, 'limit': limit, 'price': price, 'open_at': open_at})
 
         # make GET request to Yelp API using parameters
-        response = urllib2.urlopen('https://api.yelp.com/v3/businesses/search?%s' % params).read()
-        print(response)
-        return
+        url = urllib2.Request("https://api.yelp.com/v3/businesses/search?%s" % params)
+        url.add_header('Authorization', 'Bearer hJ9D0lMCziUpj-OSDaiqXc2noXKoPylRe76MsfN63jj60RSJzHMf-Fegf_rJOLQ_eN1zebXB-3E7aO0ZLTx8aeYTnqfr8halD7Te8PES9OD8_9CTWsLhPDy9a6JaWnYx')
+        json_response = json.loads(urllib2.urlopen(url).read())
+        # print(json_response)
+
+        restaurants = json_response["businesses"]
+        # add each restaurant to Firebase
+        for restaurant in restaurants:
+            print(restaurant)
+        abort(404)
     if request.method == 'GET':
         abort(404)
     abort(404)
