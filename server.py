a3ef3dac677a6c2eb19b3f9cd4a74a1db6bc16ac
rwblickhan <rwblickhan@gmail.com>
2018-01-15 20:37:32 -0800

    Cleanup

--- server.py
+++ server.py
@@ -60,7 +60,12 @@ def options():
         price = request.args.get("price")
         open_at = request.args.get("open_at")
 
-        if not location or not radius or not categories or not limit or not price or not open_at:
+        if (not location or
+                not radius or
+                not categories or
+                not limit or
+                not price or
+                not open_at):
             abort(Response(status=400))
 
         params = urllib.parse.urlencode({'location': location,
